2118d7b92fe59d3de9b1b3b599fac1963a9c452b
Nikolaos Vovos <nikolaos.vovos@prometric.com>
2025-05-23 19:08:54 +0100

    fixes registration

--- alumni-tracker/src/pages/register/register.component.ts
+++ alumni-tracker/src/pages/register/register.component.ts
@@ -1,11 +1,33 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [RouterModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent {}
+export class RegisterComponent {
+  username = '';
+  password = '';
+  errorMessage = '';
+
+  constructor(private http: HttpClient, private router: Router) {}
+
+  register() {
+    debugger;
+    this.http.post('https://localhost:5001/api/account/register', {
+      username: this.username,
+      password: this.password
+    }).subscribe({
+      next: () => this.router.navigate(['/']),
+      error: (error) => {
+        this.errorMessage = error.error?.error || 'Registration failed';
+      }
+    });
+  }
+}
