7bdb2f57ee80948bbb3326a6313afe9ce350cda0
GeorgeVovos <George.Vovos@prometric.com>
2025-06-08 21:36:02 +0100

    Small fix for IIS deployment

---
+++ alumni-tracker/public/assets/leaflet/README.txt
@@ -0,0 +1,8 @@
+/*
+  This file is required for Leaflet's default marker icons to work when deployed with Angular.
+  Place the following files in this folder:
+    - marker-icon.png
+    - marker-icon-2x.png
+    - marker-shadow.png
+  You can get them from node_modules/leaflet/dist/images/
+*/
--- alumni-tracker/src/pages/graduates-map/graduates-map.component.ts
+++ alumni-tracker/src/pages/graduates-map/graduates-map.component.ts
@@ -16,6 +16,7 @@ export class GraduatesMapComponent implements AfterViewInit, OnChanges {
   @Input() currentUserId!: number;
 
   private L: any;
+  private leaflet: any;
   private map: any;
   private mapReady = false;
   private markers: any[] = [];
@@ -24,13 +25,20 @@ export class GraduatesMapComponent implements AfterViewInit, OnChanges {
     if (typeof window === 'undefined') return;
 
     this.L = await import('leaflet');
-
-    this.map = this.L.map('map').setView([20, 0], 2);
-
-    this.L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    this.leaflet = this.L.default ? this.L.default : this.L;
+    // Defensive: Use Icon.Default from the correct object
+    const IconDefault = this.leaflet.Icon?.Default;
+    if (IconDefault) {
+      IconDefault.mergeOptions({
+        iconRetinaUrl: 'assets/leaflet/marker-icon-2x.png',
+        iconUrl: 'assets/leaflet/marker-icon.png',
+        shadowUrl: 'assets/leaflet/marker-shadow.png',
+      });
+    }
+    this.map = this.leaflet.map('map').setView([20, 0], 2);
+    this.leaflet.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '&copy; OpenStreetMap contributors'
     }).addTo(this.map);
-
     this.mapReady = true;
     this.plotMarkers();
   }
@@ -70,8 +78,8 @@ export class GraduatesMapComponent implements AfterViewInit, OnChanges {
         ? 'https://maps.google.com/mapfiles/ms/icons/green-dot.png'
         : 'https://maps.google.com/mapfiles/ms/icons/red-dot.png';
 
-      const marker = this.L.marker([lat, lng], {
-        icon: this.L.icon({
+      const marker = this.leaflet.marker([lat, lng], {
+        icon: this.leaflet.icon({
           iconUrl,
           iconSize: [32, 32],
           iconAnchor: [16, 32],
